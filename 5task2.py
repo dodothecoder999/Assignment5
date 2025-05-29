0d0e4e247a1e868eb0b2285fcdcd90488ae5f6fa
saniya koyal <dodoprogramming019@gmail.com>
2025-05-29 15:25:25 +0530

    Update 5task2.py

--- 5task2.py
+++ 5task2.py
@@ -6,3 +6,9 @@ print("Extracted First five element:",a)
 
 print("Reversed Extracted list:",a[::-1])
 
+OUTPUT
+PS C:\Users\SANIYA\OneDrive\Desktop\dodothecoder> & "C:/Program Files/Python313/python.exe" c:/Users/SANIYA/OneDrive/Desktop/dodothecoder/hii.py
+Original list: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+Extracted First five element: [1, 2, 3, 4, 5]
+Reversed Extracted list: [5, 4, 3, 2, 1]
+
