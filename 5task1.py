6344833fb53cbfddb26c776c1be47378982f7245
saniya koyal <dodoprogramming019@gmail.com>
2025-05-29 12:35:15 +0530

    Update 5task1.py

--- 5task1.py
+++ 5task1.py
@@ -5,3 +5,12 @@ if a in dict:
     print(""+a+"'s Marks:",dict[a])
 else:
     print("Student name not found")
+
+OUTPUT
+s/Python313/python.exe" c:/Users/SANIYA/OneDrive/Desktop/dodothecoder/5task1.py
+Enter students name:Alfiya
+Alfiya's Marks: 67
+
+PS C:\Users\SANIYA\OneDrive\Desktop\dodothecoder> & "C:/Program Files/Python313/python.exe" c:/Users/SANIYA/OneDrive/Desktop/dodothecoder/5task1.py
+Enter students name:Nisha
+Student name not found
